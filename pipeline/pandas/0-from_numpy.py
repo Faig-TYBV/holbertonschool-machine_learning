984e5ee93086f574b395eed71ad20123928a79bd
Faig-TYBV <tayibovfaig.com>
2026-01-09 23:40:48 +0000

    from numpy array to a dataframe

---
+++ pipeline/pandas/0-from_numpy.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" Learning pandas """
+import pandas
+
+
+def from_numpy(array):
+    lst = [chr(ord('A') + i) for i in range(array.shape[1])]
+    return pd.DataFrame(array, columns=lst)
