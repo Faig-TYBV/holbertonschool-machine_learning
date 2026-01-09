aa313331d4435e71b43bd202b522c27fb4363703
Faig-TYBV <tayibovfaig.com>
2026-01-09 23:42:53 +0000

    from numpy array to a dataframe

--- pipeline/pandas/0-from_numpy.py
+++ pipeline/pandas/0-from_numpy.py
@@ -4,5 +4,6 @@ import pandas as pd
 
 
 def from_numpy(array):
+    """ from array to dataframe """
     lst = [chr(ord('A') + i) for i in range(array.shape[1])]
     return pd.DataFrame(array, columns=lst)
