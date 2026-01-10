8983d8a8a714365d85ccd010dec664c4c5bf5e59
Faig-TYBV <tayibovfaig.com>
2026-01-10 00:32:36 +0000

    taking a slice from our dataframe

---
+++ pipeline/pandas/5-slice.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+""" taking a slice from dataframe """
+
+
+def slice(df):
+    """ taking a slice from dataframe """
+
+    return df.loc[60, ["High", "Low", "Close", "Volume_BTC"]]
