5079956fbdf2e6348fd21df2441b9cf38c93187b
Faig-TYBV <tayibovfaig.com>
2026-01-14 16:26:46 +0000

    Learning Calculus

---
+++ math/calculus/10-matisse.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""Finding derivatives"""
+
+
+def poly_derivative(poly):
+    """Coefficients of the derivative of the polynomial"""
+
+    dev = []
+    i = 0
+    for ele in poly:
+        if i == 0:
+            i += 1
+            continue
+        dev.append(ele*i)
+        i += 1
+    if len(dev) == 0:
+        dev.append(0)
+    return dev
