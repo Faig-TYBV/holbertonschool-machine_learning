d4b8b00502bdefe94810627c9023aa42c33ea927
Faig-TYBV <tayibovfaig.com>
2026-01-29 00:37:53 +0000

    adding arrays

---
+++ math/linear_algebra/4-line_up.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+'''finding sum of 2 arrays'''
+
+
+def add_arrays(arr1, arr2):
+    '''finding sum of 2 arrays'''
+    if len(arr2) != len(arr1):
+        return None
+    return arr1 + arr2
