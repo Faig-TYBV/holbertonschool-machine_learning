5113336eb8f1267970403103ff238c5f3b854f0b
Faig-TYBV <tayibovfaig.com>
2026-01-29 10:42:44 +0000

    matrix multipication

--- math/linear_algebra/8-ridin_bareback.py
+++ math/linear_algebra/8-ridin_bareback.py
@@ -11,7 +11,7 @@ def mat_mul(mat1, mat2):
     for row in mat1:
         mat = []
         for j in range(len(mat2[0])):
-            sm = 0;
+            sm = 0
             for r in range(len(row)):
                 sm = sm + row[r] * mat2[r][j]
             mat.append(sm)
