61d2f5b5923a0e3dfbbdcf45e37de076c6bbb191
Faig-TYBV <tayibovfaig.com>
2026-01-29 11:14:33 +0000

    matrix multipication

---
+++ math/linear_algebra/14-saddle_up.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+'''matrix multipication by using numpy'''
+import numpy as np
+
+
+def np_matmul(mat1, mat2):
+    '''multipication of matrixes'''
+
+    return np.matmul(mat1, mat2)
