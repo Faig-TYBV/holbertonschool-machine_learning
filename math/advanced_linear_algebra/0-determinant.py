5e5a9ac462aa735905fe9adfa11234c2291a5ee8
ftayibov20472 <ftayibov20472@ada.edu.az>
2026-02-08 13:32:03 +0400

    calculating determinant of the matrix

--- math/advanced_linear_algebra/0-determinant.py
+++ math/advanced_linear_algebra/0-determinant.py
@@ -6,6 +6,7 @@ Docstring for holbertonschool-machine_learning.math.advanced_linear_algebra.0-de
 
 def determinant(matrix):
     '''Calculates the determinant of a matrix:'''
+
     # 1. Validation: Check if matrix is a list of lists
     if not isinstance(matrix, list) or not all(isinstance(row, list) for row in matrix):
         raise TypeError("matrix must be a list of lists")
