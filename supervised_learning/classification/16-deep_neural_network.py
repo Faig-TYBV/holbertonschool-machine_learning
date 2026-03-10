afd01ec9cc19975095950bb766d3ece5742e9a0a
ftayibov20472 <ftayibov20472@ada.edu.az>
2026-03-11 03:28:15 +0400

    Deep Neural Network class

---
+++ supervised_learning/classification/16-deep_neural_network.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+"""Deep Neural Network class"""
+
+
+import numpy as np
+
+
+class DeepNeuralNetwork:
+    '''Deep Neural Network class'''
+
+    def __init__(self, nx, layers):
+        '''Constructor method for DeepNeuralNetwork class'''
+
+        if not isinstance(nx, int):
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+        if not isinstance(layers, list):
+            raise TypeError("layers must be a list of positive integers")
+        if min(layers) < 1:
+            raise ValueError("layers must be a list of positive integers")
+        self.L = len(layers)
+        self.cache = {}
+        '''He et al. initialization'''
+        for i in range(self.L):
+            if i == 0:
+                vector = np.random.normal(0, 1, (layers[i], nx))
+                self.weights['W' + str(i + 1)] = vector
+                self.weights['W' + str(i + 1)] *= np.sqrt(2 / nx)
+            else:
+                vector = np.random.normal(0, 1, (layers[i], layers[i - 1]))
+                self.weights['W' + str(i + 1)] = vector
+                self.weights['W' + str(i + 1)] *= np.sqrt(2 / layers[i - 1])
+            self.weights['b' + str(i + 1)] = np.zeros((layers[i], 1))
