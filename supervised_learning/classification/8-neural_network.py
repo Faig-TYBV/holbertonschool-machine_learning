359f95e1a3f7f25e5819c3313a0e56bb9b388f32
ftayibov20472 <ftayibov20472@ada.edu.az>
2026-03-11 01:02:04 +0400

    Neural Network class

--- supervised_learning/classification/8-neural_network.py
+++ supervised_learning/classification/8-neural_network.py
@@ -11,11 +11,11 @@ class NeuralNetwork:
     def __init__(self, nx, nodes):
         '''Constructor method for NeuralNetwork class'''
 
-        if nx is not int:
+        if not isinstance(nx, int):
             raise TypeError("nx must be an integer")
         if nx < 1:
             raise ValueError("nx must be a positive integer")
-        if nodes is not int:
+        if not isinstance(nodes, int):
             raise TypeError("nodes must be an integer")
         if nodes < 1:
             raise ValueError("nodes must be a positive integer")
