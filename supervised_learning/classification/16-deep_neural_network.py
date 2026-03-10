3b20b043d8a4f5ac6ffba202f3b5b9576c64e2a2
ftayibov20472 <ftayibov20472@ada.edu.az>
2026-03-11 03:33:27 +0400

    Deep Neural Network class

--- supervised_learning/classification/16-deep_neural_network.py
+++ supervised_learning/classification/16-deep_neural_network.py
@@ -18,7 +18,7 @@ class DeepNeuralNetwork:
         if not isinstance(layers, list) or len(layers) == 0:
             raise TypeError("layers must be a list of positive integers")
         if min(layers) < 1:
-            raise ValueError("layers must be a list of positive integers")
+            raise TypeError("layers must be a list of positive integers")
         self.L = len(layers)
         self.cache = {}
         self.weights = {}
