033bd7ff5adff141c1e8ebd30255a7b79d6b96ef
ftayibov20472 <ftayibov20472@ada.edu.az>
2026-03-10 23:52:13 +0400

    Neuron class

---
+++ supervised_learning/classification/0-neuron.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""Neuron class"""
+
+
+import numpy as np
+
+
+class Neuron:
+    """Neuron class"""
+
+    def __init__(self, nx):
+        '''Constructor method for Neuron class'''
+
+        if not isinstance(nx, int):
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+        self.__W = np.random.normal(0, 1, (1, nx))
+        self.__b = 0
+        self.__A = 0
