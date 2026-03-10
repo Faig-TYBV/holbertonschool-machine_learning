8f57c2e28cc024fa1d5bba4619b6e4428dad8d86
ftayibov20472 <ftayibov20472@ada.edu.az>
2026-03-11 00:12:51 +0400

    adding evaluate function

---
+++ supervised_learning/classification/4-neuron.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3
+"""Neuron class"""
+
+
+import numpy as np
+
+
+class Neuron:
+    """Neuron class"""
+
+    def __init__(self, nx):
+        '''Constructor method for Neuron class'''
+
+        if not isinstance(nx, int):
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+        self.__W = np.random.normal(0, 1, (1, nx))
+        self.__b = 0
+        self.__A = 0
+
+    @property
+    def W(self):
+        '''W getter method'''
+
+        return self.__W
+
+    @property
+    def b(self):
+        '''b getter method'''
+
+        return self.__b
+
+    @property
+    def A(self):
+        '''A getter method'''
+
+        return self.__A
+
+    def forward_prop(self, X):
+        '''Calculates the forward propagation of the neuron'''
+
+        Z = np.matmul(self.__W, X) + self.__b
+        self.__A = 1 / (1 + np.exp(-Z))
+        return self.__A
+
+    def cost(self, Y, A):
+        ''''Calculates the cost of the model using logistic regression'''
+
+        cost = np.sum(Y * np.log(A) + (1 - Y) * np.log(1.0000001 - A))
+        return -cost / Y.shape[1]
+
+    def evaluate(self, X, Y):
+        '''Evaluates the neuron’s predictions'''
+
+        A = self.forward_prop(X)
+        cost = self.cost(Y, A)
+        return np.where(A >= 0.5, 1, 0), cost
