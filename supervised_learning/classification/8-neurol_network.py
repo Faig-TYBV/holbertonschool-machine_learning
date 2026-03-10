b41d668697820c5e8c7885df6d18b2f580c62902
ftayibov20472 <ftayibov20472@ada.edu.az>
2026-03-11 00:58:44 +0400

    Neural Network class

---
+++ supervised_learning/classification/8-neurol_network.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""Neural Network class"""
+
+
+import numpy as np
+
+
+class NeuralNetwork:
+    '''Neural Network class'''
+
+    def __init__(self, nx, nodes):
+        '''Constructor method for NeuralNetwork class'''
+
+        if nx is not int:
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+        if nodes is not int:
+            raise TypeError("nodes must be an integer")
+        if nodes < 1:
+            raise ValueError("nodes must be a positive integer")
+        self.W1 = np.random.normal(0, 1, (nodes, nx))
+        self.b1 = np.zeros((nodes, 1))
+        self.A1 = 0
+        self.W2 = np.random.normal(0, 1, (nodes, nx))
+        self.b2 = np.zeros((nodes, 1))
+        self.A2 = 0
