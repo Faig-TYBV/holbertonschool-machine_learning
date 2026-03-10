8e3cf93d9ee1ad59b1314f9ddad1a11c8d527a31
ftayibov20472 <ftayibov20472@ada.edu.az>
2026-03-11 03:42:43 +0400

    adding cost method

--- supervised_learning/classification/19-deep_neural_network.py
+++ supervised_learning/classification/19-deep_neural_network.py
@@ -69,6 +69,6 @@ class DeepNeuralNetwork:
         '''Calculates the cost of the model using logistic regression'''
 
         m = Y.shape[1]
-        cost = - (1 / m) * np.sum(Y * np.log(A) + (1 - Y) * np.log(1.0000001 - A))
+        cost = - (1 / m)
+        cost *= np.sum(Y * np.log(A) + (1 - Y) * np.log(1.0000001 - A))
         return cost
-
