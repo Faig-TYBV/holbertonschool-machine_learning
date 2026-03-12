de4afa788fba4d21e1b2e3a100cf3c0d5f2cb747
ftayibov20472 <ftayibov20472@ada.edu.az>
2026-03-13 00:47:39 +0400

    sequential model with Keras

---
+++ supervised_learning/keras/0-sequential.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""0-sequential.py
+    - Create a sequential model with Keras
+"""
+
+
+import tensorflow.keras as K
+
+
+def build_model(nx, layers, activations, lambtha, keep_prob):
+    '''build_model - creates a sequential model with Keras'''
+
+    model = K.Sequential()
+    for i in range(len(layers)):
+        if i == 0:
+            model.add(K.layers.Dense(layers[i], input_shape=(nx,), activation=activations[i], kernel_regularizer=K.regularizers.l2(lambtha)))
+        else:
+            model.add(K.layers.Dense(layers[i], activation=activations[i], kernel_regularizer=K.regularizers.l2(lambtha)))
+        if i < len(layers) - 1:
+            model.add(K.layers.Dropout(1 - keep_prob))
+    return model
