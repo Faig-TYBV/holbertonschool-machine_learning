7b400d9f96c5ba4514f62061b77c5e5fa30f7893
ftayibov20472 <ftayibov20472@ada.edu.az>
2026-03-17 13:34:53 +0400

    training the model

---
+++ supervised_learning/keras/5-train.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+'''5-train.py
+    - Train a model with Keras
+'''
+
+
+import tensorflow.keras as K
+
+
+def train_model(network, data, labels, batch_size, epochs,
+                validation_data=None, verbose=True, shuffle=False):
+    '''train_model - trains a model using mini-batch gradient descent'''
+
+    return network.fit(x=data, y=labels, batch_size=batch_size,
+                       epochs=epochs, validation_data=validation_data,
+                       verbose=verbose, shuffle=shuffle)
