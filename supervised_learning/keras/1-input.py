628f5b843853eb651565747f77c081d2b71fc250
ftayibov20472 <ftayibov20472@ada.edu.az>
2026-03-17 13:15:23 +0400

    debugging

--- supervised_learning/keras/1-input.py
+++ supervised_learning/keras/1-input.py
@@ -8,14 +8,21 @@ import tensorflow.keras as K
 
 
 def build_model(nx, layers, activations, lambtha, keep_prob):
-    '''build_model - creates a sequential model with Keras'''
+    '''build_model - creates a model with Keras Functional API'''
 
-    model = K.Sequential()
-    model.add(K.layers.Input(shape=(nx,)))
+    # 1. Define the input layer explicitly
+    inputs = K.Input(shape=(nx,))
+    # 2. Chain the layers
+    x = inputs
     for i in range(len(layers)):
-        model.add(K.layers.Dense(layers[i], activation=activations[i],
-                                 kernel_regularizer=K.
-                                 regularizers.l2(lambtha)))
-        if i < len(layers) - 1 and layers[i] != 1:
-            model.add(K.layers.Dropout(1 - keep_prob))
+        x = K.layers.Dense(
+            layers[i],
+            activation=activations[i],
+            kernel_regularizer=K.regularizers.l2(lambtha)
+        )(x)
+        # Add dropout if it's not the last layer
+        if i < len(layers) - 1:
+            x = K.layers.Dropout(1 - keep_prob)(x)
+    # 3. Create the model by connecting inputs to outputs
+    model = K.Model(inputs=inputs, outputs=x)
     return model
