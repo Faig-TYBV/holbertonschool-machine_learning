f62778851e42646251f3440b284bad5c3b5cbd41
ftayibov20472 <ftayibov20472@ada.edu.az>
2026-03-21 05:10:32 +0400

    Updates a variable using the RMSProp optimization algorithm.

--- supervised_learning/optimization/7-RMSProp.py
+++ supervised_learning/optimization/7-RMSProp.py
@@ -10,5 +10,5 @@ def update_variables_RMSProp(alpha, beta2, epsilon, var, grad, s):
     """Updates a variable using the RMSProp optimization algorithm."""
 
     s = beta2 * s + (1 - beta2) * grad ** 2
-    var = var - alpha * grad / (tf.sqrt(s) + epsilon)
+    var = var - alpha * grad / (np.sqrt(s) + epsilon)
     return var, s
