dc1d893ef56d289f7b1db6d224b8e6db53fb2955
ftayibov20472 <ftayibov20472@ada.edu.az>
2026-03-22 17:17:00 +0400

    L3 regularization create layer function

---
+++ supervised_learning/regularization/3-l2_reg_create_layer.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+"""L3 regularization create layer function
+"""
+
+
+import tensorflow as tf
+
+
+def l2_reg_create_layer(prev, n, activation, lambtha):
+    """
+    Creates a neural network layer in TensorFlow with L2 regularization.
+
+    Args:
+        prev: Tensor containing the output of the previous layer.
+        n: Number of nodes the new layer should contain.
+        activation: Activation function to be used on the layer.
+        lambtha: L2 regularization parameter.
+
+    Returns:
+        The output of the new layer.
+    """
+
+    # Define the L2 regularizer with the given lambtha
+    regularizer = tf.keras.regularizers.L2(l2=lambtha)
+    # Initialize weights using He normal initialization (common practice
+    #  for deep networks)
+    initializer = tf.keras.initializers.VarianceScaling(scale=2.0,
+                                                        mode='fan_avg',
+                                                        distribution='uniform')
+    # Create the Dense layer
+    layer = tf.keras.layers.Dense(
+        units=n,
+        activation=activation,
+        kernel_regularizer=regularizer,
+        kernel_initializer=initializer
+    )
+    return layer(prev)
