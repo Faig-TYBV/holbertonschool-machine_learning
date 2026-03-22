129db6baaa70d885143ab019905063bf1d727b9f
ftayibov20472 <ftayibov20472@ada.edu.az>
2026-03-22 17:33:14 +0400

    early stopping

--- supervised_learning/regularization/7-early_stopping.py
+++ supervised_learning/regularization/7-early_stopping.py
@@ -16,10 +16,10 @@ def early_stopping(cost, opt_cost, threshold, patience, count):
         bool - whether to stop early, and the updated count
     """
 
-    if cost - opt_cost >= threshold:
-        count += 1
-    else:
+    if opt_cost - cost > threshold:
         count = 0
+    else:
+        count += 1
     if count >= patience:
         return True, count
     return False, count
