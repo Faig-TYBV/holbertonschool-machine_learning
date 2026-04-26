f9244aac6b754a0d20e016f9ebe35ab66af6f211
ftayibov20472 <ftayibov20472@ada.edu.az>
2026-04-26 14:05:35 +0400

    conv_backward

--- supervised_learning/cnn/2-conv_backward.py
+++ supervised_learning/cnn/2-conv_backward.py
@@ -1,45 +1,88 @@
 #!/usr/bin/env python3
-'''2-conv_backward.py'''
-
+'''
+convolutional back propogation
+'''
 
 import numpy as np
 
 
 def conv_backward(dZ, A_prev, W, b, padding="same", stride=(1, 1)):
-    '''performs back propagation over a convolutional layer of a neural network
     '''
+    dZ - a numpy.ndarray of shape (m, h_new, w_new, c_new) containing
+            the partial derivatives with respect to the unactivated output
+            of the convolutional layer
+        m - the number of examples
+        h_new - the height of the output
+        w_new - the width of the output
+        c_new - the number of channels in the output
+    A_prev - a numpy.ndarray of shape (m, h_prev, w_prev, c_prev) containing
+                the output of the previous layer
+        h_prev - the height of the previous layer
+        w_prev - the width of the previous layer
+        c_prev - the number of channels in the previous layer
+    W - a numpy.ndarray of shape (kh, kw, c_prev, c_new) containing
+            the kernels for the convolution
+        kh - the kernel height
+        kw - the kernel width
+    b - a numpy.ndarray of shape (1, 1, 1, c_new) containing
+            the biases applied to the convolution
+    padding - a string that is either same or valid,
+                indicating the type of padding used
+    stride - a tuple of (sh, sw) containing the strides for the convolution
+        sh - the stride for the height
+        sw - the stride for the width
 
+    Returns: dA_prev, dW, db
+    dA_prev is a numpy.ndarray containing
+    the partial derivatives with respect to A_prev,
+                has the same shape as A_prev
+    dW is a numpy.ndarray containing the partial derivatives with respect to W,
+                has the same shape as W
+    db is a numpy.ndarray containing the partial derivatives with respect to b,
+                has the same shape as b
+    '''
     m, h_prev, w_prev, c_prev = A_prev.shape
     kh, kw, _, c_new = W.shape
-    _, h_new, w_new, _ = dZ.shape
     sh, sw = stride
-    if padding == "same":
-        ph = max((h_prev - 1) * sh + kh - h_prev, 0)
-        pw = max((w_prev - 1) * sw + kw - w_prev, 0)
-        pad_top,  pad_bottom = ph // 2, ph - ph // 2
-        pad_left, pad_right = pw // 2, pw - pw // 2
-        A_prev_pad = np.pad(
-            A_prev,
-            ((0, 0), (pad_top, pad_bottom), (pad_left, pad_right), (0, 0)),
-            mode="constant"
-        )
+    _, h_new, w_new, _ = dZ.shape
+    if padding == 'same':
+        ph = ((h_prev - 1) * sh + kh - h_prev) // 2 + 1
+        pw = ((w_prev - 1) * sw + kw - w_prev) // 2 + 1
     else:
-        pad_top = pad_bottom = pad_left = pad_right = 0
-        A_prev_pad = A_prev
+        ph, pw = 0, 0
+
+    A_prev_pad = np.pad(A_prev, ((0,), (ph,), (pw,), (0,)), mode='constant')
     dA_prev_pad = np.zeros_like(A_prev_pad)
+    dA_prev = np.zeros_like(A_prev)
     dW = np.zeros_like(W)
-    db = np.sum(dZ, axis=(0, 1, 2), keepdims=True)
-    for i in range(h_new):
-        for j in range(w_new):
-            hs, ws = i * sh, j * sw
-            dz = dZ[:, i, j, :]
-            a_slice = A_prev_pad[:, hs:hs+kh, ws:ws+kw, :]
-            dW += np.tensordot(a_slice, dz, axes=([0], [0]))
-            dA_prev_pad[:, hs:hs+kh, ws:ws+kw, :] += (
-                np.tensordot(dz, W, axes=([1], [3]))
-            )
-    # ── strip padding safely, avoiding the -0 pitfall ──────────────────────
-    h_end = None if pad_bottom == 0 else -pad_bottom
-    w_end = None if pad_right == 0 else -pad_right
-    dA_prev = dA_prev_pad[:, pad_top:h_end, pad_left:w_end, :]
+    db = np.zeros_like(b)
+    for i in range(m):
+        a_prev_pad = A_prev_pad[i]
+        da_prev_pad = dA_prev_pad[i]
+        for h in range(h_new):
+            for w in range(w_new):
+                for c in range(c_new):
+                    vert_start = h * sh
+                    vert_end = vert_start + kh
+                    horiz_start = w * sw
+                    horiz_end = horiz_start + kw
+
+                    a_slice = a_prev_pad[vert_start:vert_end,
+                                         horiz_start:horiz_end, :]
+                    da_prev_pad[vert_start:vert_end,
+                                horiz_start:horiz_end, :] += W[:,
+                                                               :,
+                                                               :,
+                                                               c] * dZ[i,
+                                                                       h,
+                                                                       w,
+                                                                       c]
+                    dW[:, :, :, c] += a_slice * dZ[i, h, w, c]
+                    db[:, :, :, c] += dZ[i, h, w, c]
+
+        if padding == 'same':
+            dA_prev[i, :, :, :] = da_prev_pad[ph:-ph, pw:-pw, :]
+        else:
+            dA_prev[i, :, :, :] = da_prev_pad
+
     return dA_prev, dW, db
