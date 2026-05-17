a0b7216de0fae5bfe0b1e3ecca8ab8bf40789064
Faig-TYBV <tayibovfaig@gmail.com>
2026-05-17 23:46:51 +0400

    dimensionality reduction

--- unsupervised_learning/dimensionality_reduction/4-P_affinities.py
+++ unsupervised_learning/dimensionality_reduction/4-P_affinities.py
@@ -1,43 +1,60 @@
 #!/usr/bin/env python3
-"""A module that does the trick"""
+"""Module for calculating symmetric P affinities for t-SNE."""
+
 import numpy as np
+
 P_init = __import__('2-P_init').P_init
 HP = __import__('3-entropy').HP
 
 
 def P_affinities(X, tol=1e-5, perplexity=30.0):
-    """A function that does the trick"""
-    (n, d) = X.shape
+    """
+    Calculates the symmetric P affinities of a data set.
+
+    Args:
+        X: numpy.ndarray of shape (n, d) containing the dataset
+        tol: maximum tolerance allowed for entropy difference
+        perplexity: desired perplexity for all Gaussian distributions
+
+    Returns:
+        P: numpy.ndarray of shape (n, n) containing symmetric P affinities
+    """
+
     D, P, betas, H = P_init(X, perplexity)
 
-    if n == 0:
-        return P
+    n = X.shape[0]
+    target_H = np.log2(perplexity)
 
     for i in range(n):
-        row = D[i].copy()
-        row = np.delete(row, i, axis=0)
-        Hi, Pi = HP(row, betas[i])
-        Hdiff = Hi - H
-        b_max = None
-        b_min = None
-        while np.abs(Hdiff) > tol:
-            if Hdiff > 0:
-                b_min = betas[i, 0]
-                if b_max is None:
-                    betas[i, 0] = betas[i, 0] * 2.
+        beta_min = None
+        beta_max = None
+
+        Di = np.concatenate((D[i, :i], D[i, i + 1:]))
+
+        Hi, Pi = HP(Di, betas[i])
+
+        while np.abs(Hi - target_H) > tol:
+            if Hi > target_H:
+                beta_min = betas[i].copy()
+
+                if beta_max is None:
+                    betas[i] *= 2
                 else:
-                    betas[i, 0] = (betas[i, 0] + b_max) / 2.
+                    betas[i] = (betas[i] + beta_max) / 2
+
             else:
-                b_max = betas[i, 0]
-                if b_min is None:
-                    betas[i, 0] = betas[i, 0] / 2.
+                beta_max = betas[i].copy()
+
+                if beta_min is None:
+                    betas[i] /= 2
                 else:
-                    betas[i, 0] = (betas[i, 0] + b_min) / 2.
+                    betas[i] = (betas[i] + beta_min) / 2
 
-            Hi, Pi = HP(row, betas[i])
-            Hdiff = Hi - H
-        Pi = np.insert(Pi, i, 0)
-        P[i] = Pi
+            Hi, Pi = HP(Di, betas[i])
 
-    P = (P.T + P) / (2 * n)
+        P[i, :i] = Pi[:i]
+        P[i, i + 1:] = Pi[i:]
+
+    P = (P + P.T) / (2 * n)
+
     return P
