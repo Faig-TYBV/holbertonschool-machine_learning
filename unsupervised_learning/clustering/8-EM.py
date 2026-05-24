77f4748ab0cb90492a3abf0336cbb52a117cd861
Faig-TYBV <tayibovfaig@gmail.com>
2026-05-24 23:38:35 +0400

    almost done

--- unsupervised_learning/clustering/8-EM.py
+++ unsupervised_learning/clustering/8-EM.py
@@ -64,6 +64,6 @@ def expectation_maximization(X, k, iterations=1000, tol=1e-5,
 
     if verbose:
         print("Log Likelihood after {} iterations: {}".format(
-            i + 1, round(l, 5)))
+            i, round(l, 5)))
 
     return pi, m, S, g, l
