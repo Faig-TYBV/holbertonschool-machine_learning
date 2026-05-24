2f65bf12568494af855cac2bcf2905ff4df584a9
Faig-TYBV <tayibovfaig@gmail.com>
2026-05-24 23:37:44 +0400

    almost done

--- unsupervised_learning/clustering/3-optimum.py
+++ unsupervised_learning/clustering/3-optimum.py
@@ -13,17 +13,9 @@ def optimum_k(X, kmin=1, kmax=None, iterations=1000):
     """
     Tests for the optimum number of clusters by variance.
 
-    Args:
-        X: numpy.ndarray of shape (n, d) containing the dataset
-        kmin: minimum number of clusters to check, inclusive
-        kmax: maximum number of clusters to check, inclusive
-        iterations: maximum number of iterations for K-means
-
     Returns:
-        results: list containing the outputs of K-means for each cluster size
-        d_vars: list containing the difference in variance from the smallest
-                cluster size for each cluster size
-        None, None on failure
+        results: list of (C, clss) from kmeans for each k
+        d_vars: list of variance differences from the smallest k
     """
 
     if not isinstance(X, np.ndarray) or len(X.shape) != 2:
@@ -38,36 +30,39 @@ def optimum_k(X, kmin=1, kmax=None, iterations=1000):
     if not isinstance(kmax, int) or kmax <= 0:
         return None, None
 
-    if kmax <= kmin:
-        return None, None
-
-    if kmax > X.shape[0]:
+    if kmax <= kmin or kmax > X.shape[0]:
         return None, None
 
     if not isinstance(iterations, int) or iterations <= 0:
         return None, None
 
     results = []
-    variances = []
     d_vars = []
 
     for k in range(kmin, kmax + 1):
-        C, clss = kmeans(X, k, iterations)
+        result = kmeans(X, k, iterations)
+
+        if result is None:
+            return None, None
+
+        C, clss = result
 
         if C is None or clss is None:
             return None, None
 
+        results.append((C, clss))
+
+    base_var = variance(X, results[0][0])
+
+    if base_var is None:
+        return None, None
+
+    for C, _ in results:
         var = variance(X, C)
 
         if var is None:
             return None, None
 
-        results.append((C, clss))
-        variances.append(var)
-
-    base_var = variances[0]
-
-    for var in variances:
         d_vars.append(base_var - var)
 
     return results, d_vars
