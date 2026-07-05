e45557c7e77c8d1a5bb86c30e1b7b59d8d346225
Faig-TYBV <tayibovfaig@gmail.com>
2026-07-05 20:17:32 +0400

    .\4-fasttext.py

--- supervised_learning/word_embeddings/4-fasttext.py
+++ supervised_learning/word_embeddings/4-fasttext.py
@@ -1,3 +1,8 @@
+#!/usr/bin/env python3
+"""
+Module for creating, building, and training a FastText model using Gensim.
+"""
+
 from gensim.models import FastText
 
 def fasttext_model(sentences, vector_size=100, min_count=5, negative=5, window=5, cbow=True, epochs=5, seed=0, workers=1):
