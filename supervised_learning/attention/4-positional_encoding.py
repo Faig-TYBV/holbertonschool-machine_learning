347af5bd587941c9242d4a41b61fe9f8d6e7b5b4
Faig-TYBV <tayibovfaig@gmail.com>
2026-07-20 16:43:28 +0400

    .\4-positional_encoding.py

---
+++ supervised_learning/attention/4-positional_encoding.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+"""Module to calculate positional encoding for a transformer.
+"""
+import numpy as np
+
+
+def positional_encoding(max_seq_len, dm):
+    """Calculates the positional encoding for a transformer.
+
+    Args:
+        max_seq_len (int): The maximum sequence length.
+        dm (int): The model depth.
+
+    Returns:
+        numpy.ndarray: A numpy.ndarray of shape (max_seq_len, dm)
+            containing the positional encoding vectors.
+    """
+    P = np.arange(max_seq_len)[:, np.newaxis]
+    I = np.arange(dm)[np.newaxis, :]
+
+    # Calculate the continuous angles for all positions and dimensions
+    angles = P / (10000 ** ((2 * (I // 2)) / dm))
+
+    # Apply sin to even indices and cos to odd indices
+    PE = np.zeros((max_seq_len, dm))
+    PE[:, 0::2] = np.sin(angles[:, 0::2])
+    PE[:, 1::2] = np.cos(angles[:, 1::2])
+
+    return PE
