4a766fd8392d87060e1400111c423a79535015b9
Faig-TYBV <tayibovfaig@gmail.com>
2026-08-14 02:29:11 +0400

    .\0-load_env.py

--- reinforcement_learning/q_learning/0-load_env.py
+++ reinforcement_learning/q_learning/0-load_env.py
@@ -11,8 +11,8 @@ def load_frozen_lake(desc=None, map_name=None, is_slippery=False):
     Loads the pre-made FrozenLakeEnv environment from gymnasium.
 
     Parameters:
-    - desc (list of lists or list of str, optional): A custom description
-      of the map to load for the environment.
+    - desc (list of lists, optional): A custom description of the map to
+      load for the environment.
     - map_name (str, optional): The pre-made map to load.
     - is_slippery (bool, optional): Determines if the ice is slippery.
       Defaults to False.
@@ -21,15 +21,8 @@ def load_frozen_lake(desc=None, map_name=None, is_slippery=False):
     - The gymnasium environment instance for FrozenLake.
     """
     if desc is None and map_name is None:
-        # Generate a random 8x8 map if both desc and map_name are None
         desc = gym.envs.toy_text.frozen_lake.generate_random_map(size=8)
-    elif desc is not None and isinstance(desc, list) and isinstance(desc[0],
-                                                                    list):
-        # Gymnasium expects desc to be an iterable of strings.
-        # If a list of lists is provided, join the inner lists into strings.
-        desc = ["".join(row) for row in desc]
 
-    # Create and return the FrozenLake-v1 environment
     env = gym.make(
         "FrozenLake-v1",
         desc=desc,
